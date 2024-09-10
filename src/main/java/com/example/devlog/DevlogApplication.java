44c701f07adf58eb65be010d2fdf459575780e46
hyunjeong <hyunjeong9592@gmail.com>
2024-09-10 22:33:29 +0900

    Initial commit

---
+++ src/main/java/com/example/devlog/DevlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.devlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devlog/DevlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
